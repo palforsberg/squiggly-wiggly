2728aa58961e0f6700bf471bf6076bb0df060111
Pål Forsberg <pal.93@hotmail.com>
2021-12-26 15:25:18 +0100

    Added animations.

--- src/SquigglyWiggly.svelte
+++ src/SquigglyWiggly.svelte
@@ -1,11 +1,19 @@
 <script>
 	import { bez_lerp, random } from './utils'
+	import { fade } from 'svelte/transition';
 	import * as knobby from 'svelte-knobby';
 
 	const controls = knobby.panel({
+		rand: {
+			$label: 'Seed',
+			value: Math.floor(random() * 100),
+			min: 0,
+			max: 100,
+			step: 1
+		},
 		dx: {
 			$label: 'X Delta',
-			value: 40,
+			value: 30,
 			min: 0,
 			max: 70,
 			step: 0.5
@@ -14,7 +22,7 @@
 			$label: 'Y Delta',
 			value: 100,
 			min: 0,
-			max: 70,
+			max: 250,
 			step: 0.5
 		},
 		dc: {
@@ -31,65 +39,69 @@
 			max: 720,
 			step: 1
 		},
-		stepsize: {
-			$label: 'Stepsize',
-			value: 0.01,
-			min: 0.015,
-			max: 2,
-			step: 0.001
+		lineWidth: {
+			$label: 'Line Width',
+			value: 10,
+			min: 2,
+			max: 100,
+			step: 1
 		},
+		redraw: () => {
+			$controls.rand++
+		}
 	});
 	let circles = []
+	let innerWidth = 0
 
-	const rand = Math.floor(random() * 100)
-	console.log('rand', rand)
-	
-	let bezierPoints
-	
+
 	$: {
-		const points = generatePoints(100, 200, $controls.dx, $controls.dy, 3, 200, rand)
-		bezierPoints = pointsToBezier(points)
-		circles = generate($controls.stepsize, $controls.dx, $controls.dy, $controls.dc, $controls.c1)
+		if (innerWidth > 0) {
+			const points = generatePoints(100, 200, $controls.dx, $controls.dy, 20)
+			const bezierPoints = pointsToBezier(points)
+			circles = generate(bezierPoints, 0.01, $controls.dc, $controls.c1)
+			console.log('circles ', circles.length)
+		}
 	}
 
-	function generate(dt, dx, dy, dc, start_c) {
-		var circles = []
-		var vx = dx
-		var vy = dy
-		var cx = 10
-		var cy = 100
+	function generate(bezierPoints, dt, dc, start_c) {
+		var c = []
 		var cc = start_c
 		var t = 0
-		while (t < 9) {
-			cx += vx * dt
-			cy += vy * dt
+		let last = bez_lerp(0, bezierPoints)
+		const minDist = $controls.lineWidth / 4
+		while (t < bezierPoints.length - 1) {
 			cc += dc
 			t += dt
-			cx = bez_lerp(t, bezierPoints).x
-			cy = bez_lerp(t, bezierPoints).y
-			circles.push({ cx, cy, r: 20, fill: 'hsl(' + cc + ', 100%, 50%)' })
+			const p = bez_lerp(t, bezierPoints)
+			if (Math.abs(p.x - last.x) < minDist &&
+				Math.abs(p.y - last.y) < minDist) {
+				// try to limit number of circles, if distance from last is not enough, dont draw it
+				continue;
+			}
+			c.push({ key: $controls.rand, cx: p.x, cy: p.y, r: $controls.lineWidth, fill: 'hsl(' + cc + ', 100%, 50%)' })
+			last = p
+
 		}
 
-		return circles
+		return c
 	}
 
-	function generatePoints(startx, starty, dx, dy, variance_x, variance_y, rand) {
+	function generatePoints(startx, starty, dx, dy, nrPoints = 10) {
 		const points = [{ x: startx, y: starty }]
-		var true_dy = -dy
-		for (var i = 1; i < 10; i++) {
+		var true_dy = dy
+		for (var i = 1; i < nrPoints; i++) {
 			const last = points[i - 1]
-			const y = Math.min(400, Math.max(20, last.y + addVariance(true_dy, variance_y, rand, i)))
-			const x = Math.min(400, last.x + addVariance(dx, variance_x, rand, i))
-			const p = { x, y }
-			points.push(p)
+			const x = Math.min(innerWidth - 40, last.x + addVariance(dx, dx, i))
+			const y = Math.min(400, Math.max(20, last.y + addVariance(true_dy, dy, i)))
+			points.push({ x, y })
 			true_dy = -1 * true_dy
 		}
 		return points
 	}
 
-	function addVariance(val, variance, rand2, i) {
+	function addVariance(val, variance, i) {
 		const i2 = i % 5
-		const rand = random(rand2 + i)
+		const rand = random($controls.rand + i)
 		return val + (((rand * i2)  - (0.5 * i2)) * variance)
 	}
 
@@ -112,9 +124,21 @@
 		return bezier
 	}
 </script>
-
-	<svg height="500px" width="500px" fill="#eee">
-		{#each circles as c}
-			<circle {...c} />
+	<svelte:window bind:innerWidth={innerWidth}/>
+	<svg height="100%" width="100%" fill="#000">
+		{#each circles as c, i}
+			{#key c.key}
+				<circle
+					{...c}
+					in:fade="{{ delay: 500 + i*2, duration: 0 }}"
+					out:fade="{{ delay: i*2, duration: 0 }}"
+				/>
+			{/key}
 		{/each}
 	</svg>
+
+<style>
+	:global(body) {
+        background-color: black;
+    }
+</style>
